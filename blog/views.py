970b5991bc1172daa6f29950af74d542fccef8cc
Ivan Glebov <glebov.vanya@list.ru>
2020-08-17 00:04:34 +0300

    added tasks page

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,10 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
+def tasks_list(request):
+    '''task = get_object_or_404(Task, pkT)'''
+    tasks = Task.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
+    return render(request, 'blog/tasks_list.html', {'tasks': tasks})
+
+    
+
